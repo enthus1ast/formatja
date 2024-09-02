d1352b45aea89adc836acd37e06ca1b2cf5a8a0b
David Krause (enthus1ast) <krause@biochem2.uni-frankfurt.de>
2024-09-02 16:21:24 +0200

    wip on ref obj
    Signed-off-by: David Krause (enthus1ast) <krause@biochem2.uni-frankfurt.de>

--- src/formatja.nim
+++ src/formatja.nim
@@ -22,6 +22,11 @@ proc addObjImpl[T](replacers: var Replacer, name: string, obj: T) =
     let keyStr = name & "." & key 
     replacers[keyStr] = $val
 
+# TODO Fix ref obj
+# proc addObjImpl[T](replacers: var Replacer, name: string, refobj: ref T) =
+#   var obj = refobj[]
+#   addObjImpl(replacers, name, obj)
+
 macro addObj*(replacers: typed, tys: untyped): untyped =
   result = newStmtList()
   for ty in tys:
@@ -52,6 +57,7 @@ when isMainModule:
   var formatSQLITE = "{{documentName}}__{{dateStr}}.sqlite"
   var formatXLSX = "{{documentName}}__{{dateStr}}.xlsx"
   var formatCSV = "{{documentName}}-{{csvtable}}__{{dateStr}}.csv"
+  var formatStr = "{{foo.ss}} {{foo.ii}} {{foo.ff}} {{baa.ss}} {{baa.ii}} {{baa.ff}}"
   suite "formatja":
     test "simple1":
       check format(formatSQLITE, {"documentName": "FOO", "dateStr": "DATESTR"}) == "FOO__DATESTR.sqlite"
@@ -73,7 +79,6 @@ when isMainModule:
           ff: float
       var foo = Foo(ss: "FOO SS", ii: 1337, ff: 18.15)
       var baa = Baa(ss: "BAA SS", ii: 1338, ff: 18.16)
-      var formatStr = "{{foo.ss}} {{foo.ii}} {{foo.ff}} {{baa.ss}} {{baa.ii}} {{baa.ff}}"
       block:
         var replacers: Replacer 
         replacers.addObj([foo, baa])
@@ -81,4 +86,21 @@ when isMainModule:
       block:
         check formatObj(formatStr, [foo, baa]) == "FOO SS 1337 18.15 BAA SS 1338 18.16"
 
+
+    # test "format ref obj":
+    #   type
+    #     Base = ref object of RootObj
+    #       hoho: string
+    #     Child = ref object of Base
+    #       ss: string
+    #       ii: int
+    #       ff: float
+    #   var fooRef = Child(ss: "FOO SS", ii: 1337, ff: 18.15)
+    #   var baaRef = Child(ss: "BAA SS", ii: 1338, ff: 18.16)
+    #   let foo = fooRef
+    #   let baa = baaRef
+    #   check formatObj(formatStr, [foo, baa]) == "FOO SS 1337 18.15 BAA SS 1338 18.16"
+    #   # check formatObj(formatStr, [fooRef, baaRef]) == "FOO SS 1337 18.15 BAA SS 1338 18.16"
+    #   # echo formatObj(formatStr, [foo, baa]) 
+    #   # check format(formatStr, replacers) == "FOO SS 1337 18.15 BAA SS 1338 18.16"
 
